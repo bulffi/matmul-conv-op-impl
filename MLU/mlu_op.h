83be1bdc6fdfcd14f95ad8d2fe548188a191350f
bulffi <39160610+bulffi@users.noreply.github.com>
2021-04-27 09:10:04 +0000

    basic mlu support

--- MLU/mlu_op.h
+++ MLU/mlu_op.h
@@ -6,8 +6,19 @@
 #define MATMUL_CONV_MLU_OP_H
 
 #include <cstddef>
-double* cpu_matmul(double* input, double* weight, std::size_t H, std::size_t K, std::size_t W);
-double* cpu_conv2d(double* input, double* weight, std::size_t batch, std::size_t H, std::size_t W, \
-                   std::size_t C, std::size_t w_H, std::size_t w_W, std::size_t w_batch);
+#include <cstdint>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+typedef uint16_t half;
+
+// TODO：BCL接口定义
+void PowerDifferenceKernel(half* input1,half* input2,int32_t pow,half* output,int32_t dims_a);
+double* cpu_matmul(double* input, double* weight, std::size_t H, std::size_t K, std::size_t W);
+
+
+#ifdef __cplusplus
+}
+#endif
 #endif //MATMUL_CONV_MLU_OP_H
