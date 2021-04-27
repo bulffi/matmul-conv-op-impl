3b70fb8737728fe71e115f50c1f8b385ca128b5b
Zijian <39160610+bulffi@users.noreply.github.com>
2021-04-27 11:38:58 +0800

    move eigen to vcpkg

--- GPU/gpu_op.cu
+++ GPU/gpu_op.cu
@@ -1,1 +1,13 @@
-#inclde "gpu_op.h"
+#include <cstdio>
+#include "gpu_op.h"
+
+__global__ void from_cuda() {
+    printf("Hello from cuda: {%d, %d}\n", blockIdx.x, threadIdx.x);
+}
+
+
+int main() {
+    from_cuda<<<1,10>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
