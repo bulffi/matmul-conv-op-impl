7f2e1420a35087455abde0fdd6822dafd37d32d8
bulffi <39160610+bulffi@users.noreply.github.com>
2021-05-08 14:40:25 +0000

    MLU multi sram

--- test.py
+++ test.py
@@ -11,12 +11,12 @@ import gpu_op
 
 
 def cal_time(prefix, fn):
-    M = np.random.rand(1024, 100) * 0.1 - 0.05
-    N = np.random.rand(100, 1024) * 0.1 - 0.05
+    M = np.random.rand(1024, 1024) * 0.1 - 0.05
+    N = np.random.rand(1024, 1024) * 0.1 - 0.05
     R_ = np.matmul(M, N)
-    t = time.process_time()
+    t = time.time()
     res = fn(M, N)
-    elapsed_time = time.process_time() - t
+    elapsed_time = time.time() - t
     print('{} time: {}'.format(prefix, elapsed_time))
     loss = res - R_
     for i in range(12):
@@ -29,12 +29,12 @@ def cal_time(prefix, fn):
 
 
 
-cal_time('Numpy base', np.matmul)
+# cal_time('Numpy base', np.matmul)
 # cpu_op.cpu_matmul_base(M, N)
 # cal_time('CPU base', cpu_op.cpu_matmul_base)
 # cal_time('CPU transpose', cpu_op.cpu_matmul_transpose)
 # cal_time('CPU tiling', cpu_op.cpu_matmul_tiling)
-# cal_time('CPU thread tiling', cpu_op.cpu_matmul_multi_thread_tiling)
+cal_time('CPU thread tiling', cpu_op.cpu_matmul_multi_thread_tiling)
 # cal_time('GPU base', gpu_op.gpu_matmul_base)
 # cal_time('GPU SMs', gpu_op.gpu_matmul_multi_sm)
 # cal_time('GPU SMs tiling', gpu_op.gpu_matmul_multi_sm_tiling)
@@ -42,5 +42,6 @@ import mlu_op
 # R = mlu_op.mlu_matmul_base(M, N)
 # R = mlu_op.mlu_matmul_base(M, N)
 # cal_time('MLU base', mlu_op.mlu_matmul_base)
-cal_time('MLU multi core', mlu_op.mlu_matmul_multicore)
+# cal_time('MLU multi core', mlu_op.mlu_matmul_multicore)
+cal_time('MLU multi sram', mlu_op.mlu_matmul_multi_sram_wrapper)
 # print(R)
